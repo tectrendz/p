a504fc72a7faa32738499f79445d03f18eb1bc1a
tectrendz <tectrendz@tectrendz.com>
2015-08-29 19:23:49 -0700

    Strcat in c

---
+++ strcat.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+char *
+my_strcat(char *dest, const char *src)
+{
+    char *rdest = dest;
+
+    while (*dest)
+      dest++;
+    while (*dest++ = *src++)
+      ;
+    return rdest;
+}
+/*
+   Destination string should from an array
+   from: http://stackoverflow.com/questions/1335786/c-differences-between-char-pointer-and-array
+ Here's a hypothetical memory map, showing the results of the two declarations:
+
+                0x00  0x01  0x02  0x03  0x04  0x05  0x06  0x07
+    0x00008000:  'n'   'o'   'w'   ' '   'i'   's'   ' '   't'
+    0x00008008:  'h'   'e'   ' '   't'   'i'   'm'   'e'  '\0'
+        ...
+amessage:
+    0x00500000:  'n'   'o'   'w'   ' '   'i'   's'   ' '   't'
+    0x00500008:  'h'   'e'   ' '   't'   'i'   'm'   'e'  '\0'
+pmessage:
+    0x00500010:  0x00  0x00  0x80  0x00
+
+The string literal "now is the time" is stored as a 16-element array of char
+at memory address 0x00008000. This memory may not be writable; it's best 
+to assume that it's not. You should never attempt to modify the contents 
+of a string literal.
+
+The declaration
+
+char amessage[] = "now is the time";
+allocates a 16-element array of char at memory address 0x00500000 and copies
+the contents of the string literal to it. This memory is writable; you can 
+change the contents of amessage to your heart's content:
+
+strcpy(amessage, "the time is now");
+The declaration
+
+char *pmessage = "now is the time";
+allocates a single pointer to char at memory address 0x00500010 and 
+copies the address of the string literal to it.
+
+Since pmessage points to the string literal, it should not be
+ used as an argument to functions that need to modify the string contents:
+
+strcpy(amessage, pmessage);  OKAY 
+strcpy(pmessage, amessage);  NOT OKAY 
+strtok(amessage, " ");       OKAY 
+strtok(pmessage, " ");       NOT OKAY 
+scanf("%15s", amessage);     OKAY 
+scanf("%15s", pmessage);     NOT OKAY
+and so on. If you changed pmessage to point to amessage:
+
+pmessage = amessage;
+then it can be used everywhere amessage can be used.
+*/
+
+void main ()
+{
+    char *src;
+    char dest[200] = "Original Message";
+    src = (char *)malloc(sizeof(char)*20);
+    src = " Message to append";
+    printf("Str is %s\n", my_strcat(dest, src));
+}
