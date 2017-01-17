303f3f3638d2758cdcbc6020f7d3131d6dc1a512
tectrendz <tectrendz@tectrendz.com>
2017-01-17 11:24:12 -0800

    Insertion Sort
    If array is sorted then sorting is done in the order of N

--- insertion.c
+++ insertion.c
@@ -1,24 +1,39 @@
-#include<stdio.h>
-
-void in_sort(int a[], int len)
-{
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+void insertionSort(int ar_size, int *  ar) {
+    int sorted = 0;
+    int unsorted = 1;
     int i = 0;
-    printf("Array is :");
-    for (i = 0; i < len; i++) {
-        printf("%d ", a[i]);
-    }
-    printf("\n");
-    a[0] =100;
-    printf("Array is :");
-    for (i = 0; i < len; i++) {
-        printf("%d ", a[i]);
+    int j = 0;
+    int tmp;
+    for (; (unsorted < ar_size); unsorted++, sorted++) {
+        if (ar[unsorted] < ar[sorted]) {
+            tmp = ar[unsorted];
+            j = sorted;
+            while ((j >= 0) && (ar[j] > tmp)) {
+                 ar[j+1] = ar[j];
+                 j--;
+            }
+            ar[j+1] = tmp;   
+        }
     }
 }
+int main(void) {
+   
+   int _ar_size = 6;
+   int _ar[] = {5, 2, 4, 6, 8, 3};
+   int _ar_i;
+   for(_ar_i = 0; _ar_i < _ar_size; _ar_i++) { 
+        printf("%d ", _ar[_ar_i]); 
+   }
 
-
-int  main()
-{
-   int a[] = {1, 11, 2, 23, 4};
-   in_sort(a, sizeof(a)/sizeof(a[0]));
-   return 1;
+   insertionSort(_ar_size, _ar);
+   printf("\n"); 
+   for(_ar_i = 0; _ar_i < _ar_size; _ar_i++) { 
+        printf("%d ", _ar[_ar_i]); 
+   }
+   return 0;
 }
